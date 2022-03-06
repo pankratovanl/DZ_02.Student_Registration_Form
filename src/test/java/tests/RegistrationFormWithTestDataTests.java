4c047a445c38bf7adcb391908c0117c97d581959
Pankratova Nadezhda <pankratovanl@gmail.com>
2022-03-06 22:57:10 +0500

    added more TestData

--- src/test/java/tests/RegistrationFormWithTestDataTests.java
+++ src/test/java/tests/RegistrationFormWithTestDataTests.java
@@ -11,45 +11,42 @@ import static tests.TestData.*;
 
 public class RegistrationFormWithTestDataTests extends TestBase {
 
-    String firstName = "Yuriy",
-            lastName = "Gagarin";
-
     @Test
     void FillFormTest() {
 
         open("/automation-practice-form");
 
         //заполнение формы
-        $(".main-header").shouldHave(text("Practice Form"));
+        $(".main-header").shouldHave(text(pageName));
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $(byText("Male")).click();
+        $(byText(sex)).click();
         $("#userNumber").setValue(phone);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("2");
         $(".react-datepicker__year-select").selectOptionByValue("1934");
         $(".react-datepicker__day--009").click();
-        $("#subjectsInput").setValue("Arts").pressEnter();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("img/familyguy (2).png");
-        $("#currentAddress").setValue("Address and street 1");
-        $("#react-select-3-input").setValue("NCR").pressEnter();
-        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $(byText(hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath(fileNameLocation);
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").click();
 
         //проверка заполненной формы на валидность
         $(".table-responsive").shouldHave(
                 text(firstName + " " + lastName),
-                text("gagarin@comp.ru"),
-                text("Male"),
-                text("2121212121"),
+                text(email),
+                text(sex),
+                text(phone),
                 text("09 March,1934"),
-                text("Arts"),
-                text("Music"),
-                text("familyguy (2).png"),
-                text("Address and street 1"),
-                text("NCR Delhi")
+                text(subject),
+                text(hobbies),
+                text(fileName),
+                text(currentAddress),
+                text(state + " " + city)
         );
 
     }
--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -2,6 +2,18 @@ package tests;
 
 public class TestData {
     public static String
+            pageName = "Practice Form",
+            firstName = "Yuriy",
+            lastName = "Gagarin",
             email = "gagarin@comp.ru",
-            phone = "2121212121";
+            sex = "Male",
+            phone = "2121212121",
+            subject = "Arts",
+            hobbies = "Music",
+            fileNameLocation = "img/familyguy (2).png",
+            fileName = "familyguy (2).png",
+            currentAddress = "Address and street 1",
+            state = "NCR",
+            city = "Delhi";
+
 }

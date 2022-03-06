f448848599b93bc11021b0ebeab0ad0b43263d1a
Pankratova Nadezhda <pankratovanl@gmail.com>
2022-03-07 01:02:28 +0500

    added Faker

---
+++ src/test/java/tests/RegistrationFormWithFakerTests.java
@@ -0,0 +1,56 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static tests.TestData.*;
+
+
+public class RegistrationFormWithFakerTests extends TestBase {
+
+        @Test
+    void FillFormTest() {
+
+        open("/automation-practice-form");
+
+        //заполнение формы
+        $(".main-header").shouldHave(text(pageName));
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(sex)).click();
+        $("#userNumber").setValue(phone);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("2");
+        $(".react-datepicker__year-select").selectOptionByValue("1934");
+        $(".react-datepicker__day--009").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $(byText(hobbies)).click();
+        $("#uploadPicture").uploadFromClasspath(fileNameLocation);
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
+        $("#submit").click();
+
+        //проверка заполненной формы на валидность
+        $(".table-responsive").shouldHave(
+                text(firstName + " " + lastName),
+                text(email),
+                text(sex),
+                text(phone),
+                text("09 March,1934"),
+                text(subject),
+                text(hobbies),
+                text(fileName),
+                text(currentAddress),
+                text(state + " " + city)
+        );
+
+    }
+}
--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,20 +1,25 @@
 package tests;
 
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
 import static utils.RandomUtils.*;
 
 public class TestData {
+    public static Faker faker = new Faker(new Locale("eng"));
     public static String
             pageName = "Practice Form",
-            firstName = getRandomString(10),
-            lastName = getRandomString(10),
-            email = getRandomEmail(),
+            firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
             sex = "Male",
-            phone = getRandomPhone("+7"),
+            phone = faker.phoneNumber().subscriberNumber(10),
             subject = "Arts",
             hobbies = "Music",
             fileNameLocation = "img/familyguy (2).png",
             fileName = "familyguy (2).png",
-            currentAddress = "Address and street 1",
+            currentAddress = faker.address().fullAddress(),
             state = "NCR",
             city = "Delhi";
 
